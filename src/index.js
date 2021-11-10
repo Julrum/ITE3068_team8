ff8879d0461b3a10d7ff3051b47a358cb566ada9
Junwon Park <julrumg@gmail.com>
2021-11-10 23:09:02 +0900

    developer: Apply global style

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import './index.css';
 import App from './App';
+import GlobalStyle from './styles/globalStyle';
+import theme from './styles/theme';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ThemeProvider>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/styles/theme.js
@@ -0,0 +1,33 @@
+const calcRem = (size) => `${size / 13}rem`;
+
+const theme = {
+  fontSizes: {
+    header1: calcRem(34),
+    header2: calcRem(20),
+    body: calcRem(13),
+  },
+
+  colors: {
+    bold: '#000000',
+    main: '#333333',
+    sub: '#666666',
+    placeholder: '#999999',
+    white: '#ffffff',
+    background: '#fafafa',
+    bgDivider: '#f0f0f0',
+    disabledbg: '#f5f5f5',
+    disabledborder: '#d9d9d9',
+    boxShawdow: 'rgba(0, 0, 0, 0.25)',
+    primary: '#4D4DFF',
+    primaryDarken: '#3939EB',
+    primaryMoreDarken: '#2525D7',
+    primaryLighten: '#9D9DFF',
+    primaryTransparent: 'rgba(177, 177, 255, 0.3)',
+    secondary: '#3BAAFF',
+    danger: '#EB5757',
+    warning: '#FF9400',
+    success: '#64E981',
+  },
+};
+
+export default theme;
