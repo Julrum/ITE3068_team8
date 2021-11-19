26b343d10ab9b647593d68b07d076938f03fcfc5
방태빈 <kevinbang@hanyang.ac.kr>
2021-11-19 19:33:56 +0900

    enhancement: add dummy candidates page

---
+++ src/pages/Candidates/Candidate/Cand1.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cand1 = () => {
+    return <div>Cand1</div>;
+}
+
+export default Cand1;
--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import WithHeaderbar from '../components/templates/WithHeaderbar/WithHeaderbar';
+import { Cand1, Cand2, Cand3, Cand4, Candidates } from './Candidates';
 import Home from './Home/';
 import Info from './Info';
 
@@ -9,7 +10,12 @@ const Pages = () => (
     <WithHeaderbar>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/info" element={<Info />} />
+        <Route path="info" element={<Info />} />
+        <Route exact path="candidates" element={<Candidates />} />          
+          <Route path="candidates/cand1" element={<Cand1 />} />
+          <Route path="candidates/cand2" element={<Cand2 />} />
+          <Route path="candidates/cand3" element={<Cand3 />} />
+          <Route path="candidates/cand4" element={<Cand4 />} />
       </Routes>
     </WithHeaderbar>
   </BrowserRouter>
