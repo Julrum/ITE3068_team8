cedbd952d3c210cd0dbc42864b3108eddf1d549c
Junwon Park <julrumg@gmail.com>
2021-11-28 22:45:59 +0900

    Change NewsItem

--- src/components/molecules/NewsItem/NewsItem.js
+++ src/components/molecules/NewsItem/NewsItem.js
@@ -1,9 +1,91 @@
-import React from 'react';
-import { StyledMicroLink } from './NewsItem.style';
+import React, { useEffect, useState } from 'react';
+import mql from '@microlink/mql';
+import Skeleton from '@mui/material/Skeleton';
+
+import {
+  StyledDescription,
+  StyledImg,
+  StyledNewsItem,
+  StyledPublisher,
+  StyledPublisherIcon,
+  StyledPublisherUrl,
+  StyledTextArea,
+  StyledTitle,
+  StyledSkeleton,
+} from './NewsItem.style';
 
 const NewsItem = ({ url, large, ...rest }) => {
+  const [metadata, setMetadata] = useState();
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const fetchOG = async (url) => {
+    setMetadata(null);
+    setError(null);
+    setLoading(true);
+    const { status, data } = await mql(url);
+    if (status === 'success') {
+      setMetadata(data);
+    } else {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchOG(url);
+  }, [url]);
+
+  const handleClick = (link) => () => {
+    window.open(link);
+  };
+  console.log(loading);
   return (
-    <StyledMicroLink url={url} size={large ? 'large' : 'normal'} {...rest} />
+    <>
+      {loading && large && (
+        <>
+          <Skeleton
+            variant="rectangular"
+            width={320}
+            height={160}
+            sx={{ margin: 1 }}
+          />
+          <Skeleton width={320} height={30} sx={{ mx: 1 }} />
+          <Skeleton width="80%" sx={{ mx: 1 }} />
+          <Skeleton width="80%" sx={{ mx: 1 }} />
+          <Skeleton width="60%" sx={{ mx: 1 }} />
+        </>
+      )}
+      {loading && !large && (
+        <StyledSkeleton>
+          <Skeleton
+            variant="rectangular"
+            width={160}
+            height={160}
+            sx={{ margin: 1 }}
+          />
+          <div>
+            <Skeleton width={320} height={30} sx={{ mx: 1 }} />
+            <Skeleton width="80%" sx={{ mx: 1 }} />
+            <Skeleton width="80%" sx={{ mx: 1 }} />
+            <Skeleton width="60%" sx={{ mx: 1 }} />
+          </div>
+        </StyledSkeleton>
+      )}
+      {error && <div>내용을 불러오지 못했습니다.</div>}
+      {metadata && (
+        <StyledNewsItem large={large} onClick={handleClick(url)}>
+          <StyledImg src={metadata.image.url} large={large} alt="News Image" />
+          <StyledTextArea large={large}>
+            <StyledTitle>{metadata.title || <Skeleton />}</StyledTitle>
+            <StyledDescription>{metadata.description}</StyledDescription>
+            <StyledPublisher>
+              <StyledPublisherIcon src={metadata.logo.url} alt="" />
+              <StyledPublisherUrl>{metadata.publisher}</StyledPublisherUrl>
+            </StyledPublisher>
+          </StyledTextArea>
+        </StyledNewsItem>
+      )}
+    </>
   );
 };
 
--- src/components/molecules/NewsItem/NewsItem.style.js
+++ src/components/molecules/NewsItem/NewsItem.style.js
@@ -1,6 +1,85 @@
 import styled, { css } from 'styled-components';
 import MicroLink from '@microlink/react';
 
+export const StyledSkeleton = styled.div`
+  display: flex;
+`;
+
+export const StyledNewsItem = styled.div`
+  height: 125px;
+  margin: 10px;
+  display: flex;
+  cursor: pointer;
+  ${(props) =>
+    props.large &&
+    css`
+      height: 180px;
+      width: 320px;
+      min-width: 320px;
+      flex-direction: column;
+    `}
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const StyledImg = styled.img`
+  width: 160px;
+  object-fit: cover;
+  ${(props) =>
+    props.large &&
+    css`
+      width: 320px;
+      height: 160px;
+    `}
+`;
+
+export const StyledTitle = styled.div`
+  font-weight: bold;
+  text-align: left;
+`;
+
+export const StyledDescription = styled.div`
+  font-size: 12px;
+  min-height: 50px;
+  min-width: 50px;
+  text-overflow: clip;
+  overflow: hidden;
+  flex: 1;
+  text-align: left;
+`;
+
+export const StyledPublisher = styled.div`
+  display: flex;
+  justify-content: right;
+`;
+
+export const StyledPublisherIcon = styled.img`
+  height: ${({ theme }) => theme.fontSizes.body};
+  object-fit: cover;
+`;
+
+export const StyledPublisherUrl = styled.div`
+  min-width: 20px;
+  margin-left: 3px;
+  color: ${({ theme }) => theme.colors.sub};
+`;
+
+export const StyledTextArea = styled.div`
+  margin-left: 10px;
+  width: 100%;
+  min-width: 160px;
+  max-height: 160px;
+  display: flex;
+  flex-direction: column;
+  ${(props) =>
+    props.large &&
+    css`
+      margin-top: 10px;
+      margin-left: 0px;
+    `}
+`;
+
 export const StyledMicroLink = styled(MicroLink)`
   max-width: 100%;
   border: hidden;
