74afd273ca481bad347ab3f4f60a80a113f821ff
방태빈 <kevinbang@hanyang.ac.kr>
2021-11-30 04:09:52 +0900

    develop: update candidate cell and dummy data

--- src/components/molecules/CandIntro/CandIntro.style.js
+++ src/components/molecules/CandIntro/CandIntro.style.js
@@ -1,27 +1,35 @@
 import styled from "styled-components";
 
 export const Intro = styled.div`
+    width: 100%;
+    max-width: 100%;
 
+    word-break: keep-all;
 `;
 
 export const Name = styled.h1`
+    font-size: 5vmax;
     text-align: left;
     color: white;
     text-decoration: none;
-    margin-bottom: 0;
+    margin: 0;
 `;
 
 export const Number = styled.h4`
+    font-size: 1.8vmax;
     text-align: left;
     color: white;
     text-decoration: none;
-    margin-top: 0.3em;
-    
+    margin-top: 0.2em;
+    border-left: 0;
 `;
 
 export const Slogan = styled.h3`
+    font-size: 1.8vmax;
+    font-weight: normal;
     text-align: left;
     color: white;
     text-decoration: none;
-    
+    word-break: keep-all;
+    margin-top: 8vh;
 `
--- src/pages/Candidates/Candidate/Candidate.js
+++ src/pages/Candidates/Candidate/Candidate.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import IndivCandPage from '../../../components/templates/IndivCandPage';
 import { CandPage } from './Candidate.style';
-import profileDatas from './DummyProfile'
-import promiseDatas from './DummyPromise'
+import profileDatas from '../../../assets/dummy_data/DummyProfile';
+import promiseDatas from '../../../assets/dummy_data/DummyPromise';
 
 const Candidate = ({num}) => {
     return <CandPage>
