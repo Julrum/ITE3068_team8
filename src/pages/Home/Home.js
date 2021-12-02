e3433ae9586e1e126846e901a3fd975ecc50177f
방태빈 <kevinbang@hanyang.ac.kr>
2021-12-02 18:07:15 +0900

    bug: home background image fix & develop: update home image & d-day font

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,11 +4,12 @@ import DDay from '../../components/organisms/DDay';
 import TodayNews from '../../components/organisms/TodayNews';
 import MainRoute from '../../components/organisms/MainRoute';
 import MainSchedule from '../../components/organisms/MainSchedule';
+import backgroundimage from '../../assets/image/mainBackground2.jpg';
 
 const Home = () => {
   return (
     <StyledHome>
-      <MainContent>
+      <MainContent background={backgroundimage}>
         <DDay/>
         <TodayNews/>
         <MainRoute/>
